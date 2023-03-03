eb20b9724105cc848dd1021b3a9d0a3b82f9ca5a
Mamal Maleki <mohammadalimalekie@gmail.com>
2023-03-03 22:34:16 +0330

    Create ClassifiedAd entity according to ddd.

---
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -0,0 +1,20 @@
+﻿namespace Marketplace.Domain;
+
+public class ClassifiedAd
+{
+    public Guid Id { get; }
+
+    public ClassifiedAd(Guid? id)
+    {
+        Id = id ?? throw new ArgumentException("Identity must be specified", nameof(id));
+    }
+
+    public void SetTitle(string title) => _title = title;
+    public void UpdateText(string text) => _text = text;
+    public void UpdatePrice(decimal price) => _price = price;
+    
+    private Guid _ownerId;
+    private string _title;
+    private string _text;
+    private decimal _price;
+}
