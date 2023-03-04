8b866ca6cdc4ef99c6084a086d621262eeface82
Mamal Maleki <mohammadalimalekie@gmail.com>
2023-03-04 17:37:23 +0330

    Modify constructor to accept value objects instead of Guid

--- Marketplace.Domain/ClassifiedAd.cs
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -2,19 +2,20 @@
 
 public class ClassifiedAd
 {
-    public Guid Id { get; }
+    public ClassifiedAdId Id { get; }
 
-    public ClassifiedAd(Guid? id)
+    public ClassifiedAd(ClassifiedAdId id, UserId ownerId)
     {
-        Id = id ?? throw new ArgumentException("Identity must be specified", nameof(id));
+        Id = id;
+        _ownerId = ownerId;
     }
 
     public void SetTitle(string title) => _title = title;
     public void UpdateText(string text) => _text = text;
     public void UpdatePrice(decimal price) => _price = price;
     
-    private Guid _ownerId;
-    private string _title;
-    private string _text;
-    private decimal _price;
+    private UserId _ownerId;
+    private string? _title;
+    private string? _text;
+    private decimal? _price;
 }
